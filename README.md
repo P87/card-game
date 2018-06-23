d262b2b3995161561aedf48bf25c5d4f1bfcae5b
P87 <github@peterhul.me>
2018-06-23 11:14:58 +0100

    Initial set up of project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Card Game
+
+A project for me to play about with Mobx by creating a popular card game sometimes referred to as "Threes"
---
+++ webpack.dev.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        },{
+            test: /\.less$/,
+            loaders: ["style-loader", "css-loder", "less-loader"]
+        }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './src',
+        historyApiFallback: true
+    }
+}
